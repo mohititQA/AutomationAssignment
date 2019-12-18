cc2402a8af999d5301c886f02a677faf60d2c899
mohitit <mohit.mishraQA@gmail.com>
2019-12-18 11:01:09 +0530

    Added the test case2

--- CutePressPractice/src/test/java/TestSteps/TestCase2.java
+++ CutePressPractice/src/test/java/TestSteps/TestCase2.java
@@ -1,35 +1,111 @@
 package TestSteps;
 
+import java.io.IOException;
+
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import SetupClasses.BaseClass;
+import SetupClasses.XlUtilits;
 import pageObjects.ProductSearch;
 
 public class TestCase2 extends BaseClass {
-	ProductSearch ps;
 	
-   
-    
+	ProductSearch ps ;
 	@Test
-	public void ProductSearchResult() throws InterruptedException
+	public void openURL() throws InterruptedException
 	{
 		logger.info("****** Opening the URL ****************");
 		driver.get(super.newURL);
 		driver.manage().window().maximize();
 		Thread.sleep(4000);
+		ps  = new ProductSearch(driver);
 		
-		 ps = new ProductSearch(driver);
-		 ps.enterSKUCode("2062412");
+		 ps.LoginInApplication();
+		
+	}
+   
+    
+	@Test(dataProvider="ProductData",dependsOnMethods = { "openURL" })
+	public void ProductSearchResult(String quantity,String price,String deliveryFee, String Totalfee) throws InterruptedException
+	{
+		logger.info("Entering the quantity : " + quantity);
+		if (Integer.parseInt(quantity)>1)
+		{		
+		   ps.clickOnQuantity(quantity);
+		   for(int i=0;i<=3;i++)
+
+		   {
+
+		   	try{
+
+		   		logger.info("Entering the quantity : " + price);
+				 Assert.assertEquals(ps.getProductPrice(),price);
+				 
+				 logger.info("Entering the quantity : " + deliveryFee);
+				 Assert.assertEquals(ps.getDeliveryFee(),deliveryFee);
+				 
+				 logger.info("Entering the quantity : " + Totalfee);
+				 Assert.assertEquals(ps.getTotalPrice1(),Totalfee);
+
+		   		break;
+		   	}
+
+		   catch(Exception e)
+		   {
+
+		   	System.out.println("Exception-Element not found");
+
+		   }
+		   
+		   }
+		   
+		}
+		else{
+		 logger.info("Entering the quantity : " + price);
+		 Assert.assertEquals(ps.getProductPrice(),price);
+		 
+		 logger.info("Entering the quantity : " + deliveryFee);
+		 Assert.assertEquals(ps.getDeliveryFee(),deliveryFee);
+		 
+		 logger.info("Entering the quantity : " + Totalfee);
+		 Assert.assertEquals(ps.getTotalPrice(),Totalfee);
+		} 
+		 
 	}
 	
+	
+	
+	@DataProvider(name="ProductData")
+	String[][] requireProductData() throws IOException
+	{
+		String path= System.getProperty("user.dir")+"\\src\\test\\java\\TestData\\ProductInput.xlsx";
+		//System.out.println(path);
+		int rownum=XlUtilits.getRowCount(path, "Sheet1");
+		int colcount=XlUtilits.getCellCount(path,"Sheet1",1);
+		
+		String productData[][]=new String[rownum][colcount];
+		for(int i=1;i<=rownum;i++)
+		{
+			for(int j=0;j<colcount;j++)
+			{
+				productData[i-1][j]=XlUtilits.getCellData(path,"Sheet1", i,j);
+			}
+				
+		}
+		
+		return productData;
+	}
 	
 	@AfterTest
 	public void endSetup() 
 	{
-		
-		//driver.quit();
+		ps  = new ProductSearch(driver);
+		ps.clickRemoveButton();
+		driver.quit();
 	}
 	
 }
--- CutePressPractice/src/test/java/pageObjects/ProductSearch.java
+++ CutePressPractice/src/test/java/pageObjects/ProductSearch.java
@@ -1,5 +1,7 @@
 package pageObjects;
 
+import java.util.Set;
+
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,27 +20,135 @@ public class ProductSearch {
     	PageFactory.initElements(rdriver,this);
 	}
     
-	@FindBy(how=How.XPATH,using = "//*[@id='site_search_input']")
+	@FindBy(how=How.XPATH,using = "//*[@name='q']")
     @CacheLookup
     WebElement serachTextBox;
 	
-	@FindBy(how=How.XPATH,using = "//*[@id='modalDialog']/button")
+	@FindBy(how=How.XPATH,using = "//button[@class='vh79eN']")
     @CacheLookup
-    WebElement closeButton;
+    WebElement searchButton;
 	
-	public void enterSKUCode(String code) throws InterruptedException
+	@FindBy(how=How.XPATH,using = "//input[@class='_2zrpKA _1dBPDZ']")
+    @CacheLookup
+    WebElement userName;
+	
+	@FindBy(how=How.XPATH,using = "//input[@class='_2zrpKA _3v41xv _1dBPDZ']")
+    @CacheLookup
+    WebElement passWord;
+	
+	@FindBy(how=How.XPATH,using = "//button[@class='_2AkmmA _1LctnI _7UHT_c']")
+    @CacheLookup
+    WebElement loginButton;
+	
+	
+	@FindBy(how=How.XPATH,using = "(//div[1]/div/div/img)[1]")
+    @CacheLookup
+    WebElement firstProduct;
+	
+	@FindBy(how=How.XPATH,using = "//button[@class='_2AkmmA _2Npkh4 _2MWPVK']")
+    @CacheLookup
+    WebElement gotoCartButton;
+	
+	@FindBy(how=How.XPATH,using = "//button[@class='_2AkmmA _2Npkh4 _2MWPVK']")
+    @CacheLookup
+    WebElement addCartButton;
+	
+	@FindBy(how=How.XPATH,using = "(//div[@class='hJYgKM']//span)[1]")
+    @CacheLookup
+    WebElement price;
+	
+	@FindBy(how=How.XPATH,using = "(//div[@class='hJYgKM']//span)[2]")
+    @CacheLookup
+    WebElement deliveryFee;
+	
+	@FindBy(how=How.XPATH,using = "(//div[@class='hJYgKM']//span)[3]")
+    @CacheLookup
+    WebElement TotalPay;
+	
+	@FindBy(how=How.XPATH,using = "(//div[@class='hJYgKM']//span)[4]")
+    @CacheLookup
+    WebElement TotalPay2;
+	
+	@FindBy(how=How.XPATH,using = "(//button[@class='wNrY5O'])[2]")
+    @CacheLookup
+    WebElement quantity;
+	
+	@FindBy(how=How.XPATH,using = "(//div[@class='gdUKd9'])[2]")
+    @CacheLookup
+    WebElement removeButton;
+	
+	@FindBy(how=How.XPATH,using = "(//div[@class='gdUKd9 _3Z4XMp _2nQDKB'])")
+    @CacheLookup
+    WebElement removebutton;
+	
+	
+	//functions required for the test case
+	public void enterSKUCode(String product) throws InterruptedException
 	{
-		closeButton.click();
-		
-		Actions builder = new Actions(ldriver); 
-		
+		//closeButton.click();
+	
+			
 		serachTextBox.clear();
-		serachTextBox.sendKeys(code);
-		serachTextBox.sendKeys(Keys.ENTER);
-		
-		builder.sendKeys(Keys.ENTER);
+		serachTextBox.sendKeys(product);
+		searchButton.click();
+		Thread.sleep(4000);
+		firstProduct.click();
+		Thread.sleep(2000);
+		Set<String> allWindowHandles = ldriver.getWindowHandles();
+		for(String handle:allWindowHandles)
+		{
+			ldriver.switchTo().window(handle);
+		}
+		addCartButton.click();
+		Thread.sleep(2000);
 		
 	}
 	
 	
+	public void LoginInApplication() throws InterruptedException
+	{
+		userName.sendKeys("mohitit09@gmail.com");
+		passWord.sendKeys("Password$123");
+		loginButton.click();
+		Thread.sleep(4000);
+		enterSKUCode("Flipkart SmartBuy Military Keychain with Torch, Screwdriver, Knife & Bottle Opener Key Chain");
+	}
+	
+	
+	public void clickOnQuantity(String qty) throws InterruptedException
+	{
+			
+		quantity.click();
+		Thread.sleep(2000);
+	}
+	
+	public String getProductPrice()
+	{
+		return price.getText();
+	}
+	
+	
+	public String getDeliveryFee()
+	{
+		return deliveryFee.getText();
+	}
+	
+	//if quantity is 1
+	public String getTotalPrice()
+	{
+		return TotalPay.getText();
+	}
+	
+	//if quantity is 2
+		public String getTotalPrice1()
+		{
+			return TotalPay2.getText();
+		}
+		
+	public void clickRemoveButton()
+	{
+		removeButton.click();
+		removebutton.click();
+	}
 }
+
